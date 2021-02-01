7c5e68ec900e7134376e8049ed6ed9608b03093a
citypayorg <citypay.org@gmail.com>
2021-02-01 23:07:23 +0900

    merge

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/citypayorg/udex/backend/server"
+import "github.com/citypayorg/udex/tree/master/backend/server"
+
+// import "github.com/citypayorg/udex/backend/server"
 
 func main() {
 	server.Start()
